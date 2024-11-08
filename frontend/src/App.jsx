cf59e8ff97c3d197d3b4384c55eff37fddaa5891
TTruble <blazingaustin@gmail.com>
2024-11-08 08:37:31 +0200

    Hero page changes, added awards and promise

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,18 @@ import viteLogo from '/vite.svg'
 import './assets/App.scss'
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Promise from './components/Promise/Promise';
+import Awards from './components/Awards/Awards';
 
-  function App() {
-    return (
-      <>
-        <Header />
-        <Hero />
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Promise />
+      <Awards />
+    </>
+  );
+}
 
 export default App
