ca7db412e61a851ab794449d859ff555a7c9faef
TTruble <blazingaustin@gmail.com>
2024-11-19 13:15:33 +0200

    image working

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Contact.scss';
-import laptopImage from '../../assets/laptop-typing.png';
+import laptopImage from '../../assets/laptoptyping.png';
 
 const Contact = () => {
   return (
